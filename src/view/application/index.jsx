ade7df34c43a1a188dc02bc296b25b9c48bf2306
Alex Todoran <alex.todoran@luminos.software>
2024-01-21 21:41:46 +0200

    🥹

--- src/view/application/index.jsx
+++ src/view/application/index.jsx
@@ -1,14 +1,17 @@
 import { useEffect, useState, useRef } from 'react';
 
+import { mock } from 'Helpers';
 import DatePicker from 'Components/DatePicker';
 
 const random = (min, max) => Math.random() * (max - min) + min;
 
 const opacityDuration = 1;
 
-const emojis = ['🍔', '🍕', '🍟', '🌭', '🍿', '🍗', '🔥'];
+const Circle = () => <div className='h-10 w-10 shrink-0 bg-emerald-500 rounded-full'></div>;
 
-const Bubble = ({ id, onAnimationEnd }) => {
+const emojis = [<Circle />, '🍔', '🍕', '🍟', '🌭', '🍿', '🍗', '🔥'];
+
+const Bubble = ({ id = '', onAnimationEnd = mock }) => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
   const [opacity, setOpacity] = useState(1);
   const size = useRef(random(0.7, 1.5));
@@ -19,7 +22,7 @@ const Bubble = ({ id, onAnimationEnd }) => {
   const initialOptions = useRef({ animationDuration: random(2, 5), element, onAnimationEnd, id });
 
   useEffect(() => {
-    const { animationDuration, element, onAnimationEnd, id } = initialOptions.current;
+    const { animationDuration, element, id, onAnimationEnd } = initialOptions.current;
 
     element.current.addEventListener('transitionend', event => {
       if (event.propertyName === 'opacity') {
@@ -58,19 +61,25 @@ const Analytics = () => {
   const [value, onChange] = useState(new Date());
 
   const [likes, setLikes] = useState([]);
-  const cleanLike = useRef(id => {
-    setLikes(currentLikes => currentLikes.filter(like => like !== id));
-  });
+  const cleanLike = useRef(id => setLikes(currentLikes => currentLikes.filter(like => like !== id)));
 
   return (
-    <div className='relative mt-40 mx-40 w-20 flex items-center'>
-      <DatePicker minDate='2024-01-10' maxDate='2024-01-25' value={value} onChange={onChange} />
+    <div className='h-dvh max-h-dvh overflow-y-auto bg-green-200'>
+      <div className='relative mx-auto w-fit my-40'>
+        <DatePicker
+          minDate='2024-01-10'
+          maxDate='2024-01-25'
+          value={value}
+          onChange={date => {
+            onChange(date);
+            setLikes(prevLikes => [...prevLikes, new Date().getTime()]);
+          }}
+        />
 
-      <button onClick={() => setLikes(prevLikes => [...prevLikes, new Date().getTime()])}>Like</button>
-
-      {likes.map(id => (
-        <Bubble onAnimationEnd={cleanLike.current} key={id} id={id} />
-      ))}
+        {likes.map(id => (
+          <Bubble onAnimationEnd={cleanLike.current} key={id} id={id} />
+        ))}
+      </div>
     </div>
   );
 };
